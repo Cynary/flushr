af8a6bc5583c9811b47cd2764678d024a1a26c5b
Rodrigo Toste Gomes <rgomes@mit.edu>
2014-03-28 21:30:56 -0400

    Function for sorting bathrooms with current location done.

--- bathroomLoc.js
+++ bathroomLoc.js
@@ -1,24 +1,56 @@
+// Calculate spherical distance; from
+// http://stackoverflow.com/questions/13840516/how-to-find-my-distance-to-a-known-location-in-javascript
+function distance(lon1, lat1, alt1, lon2, lat2, alt2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+    var dLon = (lon2-lon1).toRad(); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    
+    // let's take altitude into account
+    d = Math.sqrt(d*d + (alt1-alt2)*(alt1-alt2))
+    return d;
+}
+
+/** Converts numeric degrees to radians */
+if (typeof(Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function() {
+        return this * Math.PI / 180;
+    }
+}
+
 bathrooms = [
     {
-        location: "Building 4, 2nd floor",
-        
+        locationName: "Building 4, 2nd floor",
+        latitude:,
+        longitude:,
+        altitude:,
     }
 ]
 
-function getLocation() {
+function getBathrooms(callback) {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(sortBathrooms(callback));
     } else {
-        alert("No location for you.");
+        callback(bathrooms)
     }
 }
 
-function showPosition(position) {
-    getBaths(position.coords);
+function sortBathrooms(callback) {
+    return function(position) {
+        var altitude = position.coords.altitude
+        var latitude = position.coords.latitude
+        var longitude = position.coords.longitude
+        bathrooms.sort(function(bath1, bath2) {
+            var dist1 = distance(longitude,latitude,altitude,
+                                 bath1.longitude,bath1.latitude,bath1.altitude)
+            var dist2 = distance(longitude,latitude,altitude,
+                                 bath2.longitude,bath2.latitude,bath2.altitude)
+            return dist1-dist2
+        })
+        callback(bathrooms)
+    }
 }
-
-function getBaths(loc) {
-    alert(loc.latitude + ' ' + loc.longitude);
-}
-
-getLocation();
